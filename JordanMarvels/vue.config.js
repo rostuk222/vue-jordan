5c48107fb6d1a2461475e773dba1bed68a1a22be
Rostyslav Khomitskyu <roshom68@gmail.com>
2020-05-29 15:10:14 +0300

    ref fix

--- JordanMarvels/vue.config.js
+++ JordanMarvels/vue.config.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 module.exports = {
     filenameHashing: false,
+    outputDir: path.resolve(__dirname, "../vue-jordan"),
     devServer: {
         https: true
     }
